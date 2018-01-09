90ec443efb1a8518968c073850f23609b651cc4e
Randy Hoang <randyviethoang@gmail.com>
2018-01-08 20:16:52 -0800

    Adding three files to pythonclass
    These are my work for 21A Python for programmers

---
+++ pythonclass/firsthello.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------------------------
+# Name:        firsthello
+# Purpose:     our first Python program
+#
+# Author:      Randy Hoang
+# -----------------------------------------------------------------------------
+"""
+A little python program that prints a greeting
+
+Prompt the user for their name.
+Print a customized Hello message
+"""
+
+print("Hello World")
