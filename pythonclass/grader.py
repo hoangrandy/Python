bbdc134ba424283c1dbca13daf37e7000cc9e2b8
Randy Hoang <rhoang@fb.com>
2018-01-17 10:56:10 -0800

    Added grade outputs

--- pythonclass/grader.py
+++ pythonclass/grader.py
@@ -13,9 +13,9 @@ This program will calculate the user's final grade by finding the average
 of all the user's grades and print it
 """
 
-# Prompts the user to input their grades
-grade_input = True
-grades = []
+
+grade_input = True # Prompts the user to input their grades
+grades = [] # Creates an empty table that holds the grades
 # A loop that asks the user to input their grades until they type end
 while grade_input:
     user_input = input('Enter your grade here, then click return, '
@@ -23,15 +23,26 @@ while grade_input:
 # If the user types end, this will change grade_input to false thus breaking
     if user_input == 'end':
         grade_input = False
-# If the user types a number, change it to a float
-    else:
+    else: # If the user types a number, change it to a float
         final_grade = float(user_input)
-        grades.append(final_grade)
+        grades.append(final_grade) # Insert the input into the string
 
-total_grade = sum(grades)
-number_grade = len(grades)
-average_total = total_grade / number_grade
-print(average_total)
+
+total_grade = sum(grades) # Take the grades list and add the values
+number_grade = len(grades) # Takes the number of inputs and store them here
+average_total = total_grade / number_grade # Total / number of inputs
+print('Course Average: ',average_total)
+
+if average_total < 60 and average_total > 0:
+    print('Letter Grade: F')
+elif average_total >= 60 and average_total < 70:
+    print('Letter Grade: D')
+elif average_total >= 70 and average_total < 80:
+    print('Letter Grade: C')
+elif average_total >= 80 and average_total < 90:
+    print('Letter Grade: B')
+else:
+    print ('Letter Grade: A')
 
 
 
