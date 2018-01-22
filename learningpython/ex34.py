67f1b7745d10dfb20f172e0c0378508109d576f2
Randy Hoang <rhoang@fb.com>
2018-01-21 23:49:04 -0500

    practicing with lists

---
+++ learningpython/ex34.py
@@ -0,0 +1,15 @@
+# -----------------------------------------------------------------------------
+# Name:        Let's make a simple list
+# Purpose:     Make a list and explain how it works
+#
+# Author: Randy Hoang
+# Date: 1/21/2018
+# -----------------------------------------------------------------------------
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1 where is that, in python the first starts with a '0'
+print animals[0] # bear would be the 'first' animal 0 = first index
+print animals[2] # what is the 3rd animal?
+print animals[1] # The animal at 1, would be a python
+print animals[5] # platypus
+print animals[-1] # platypus, last index is also -1, going left would -1 more
