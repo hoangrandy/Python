adc6f9dbef72f6f857b29fffdb294be5805e9ed6
Randy Hoang <rhoang@fb.com>
2018-02-05 15:04:26 -0800

    A start of a game in python 3, scrapping python 2 start

---
+++ pythonclass/dungeonmaster.py
@@ -0,0 +1,21 @@
+# -----------------------------------------------------------------------------
+# Name:        Dungeon
+# Purpose:     A Dungeon Game in python 3
+#
+# Author: Randy Hoang
+# Date: 02/05/2018
+# -----------------------------------------------------------------------------
+"""
+Dungeon Master
+
+The game in python 3
+"""
+
+
+
+
+main()
+
+
+if __name__ == '__main__':
+    main()
