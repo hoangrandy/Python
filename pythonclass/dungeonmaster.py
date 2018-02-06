5783feb41ee06568913e7776572691e18bfe7fe2
Randy Hoang <rhoang@fb.com>
2018-02-05 16:11:32 -0800

    Started off the story

--- pythonclass/dungeonmaster.py
+++ pythonclass/dungeonmaster.py
@@ -14,7 +14,29 @@ The game in python 3
 
 
 
-main()
+
+def user_input():
+    """
+    This function asks for the input then stores it in a variable
+    :return: input
+    """
+    userinput = input(' ')
+    return userinput
+
+
+def entrance():
+    print("""You wake up, everything is blurry and you do not remember what
+happened to you... You start to gain your vision back, but it feels weird.  
+Although it is blurry, you don't seem to know where you are at.""")
+    print("""You feel around, hopefully finding something that can 
+help you out.  You feel something that feels like eyedrops. """)
+
+    userinput = input('Do you take the eyedrops? ')
+    if userinput:
+        print('good for you ')
+
+def main():
+    entrance()
 
 
 if __name__ == '__main__':
