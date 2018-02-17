b7e060275eea63797ff7da3302139f4826744224
Randy Hoang <rhoang@fb.com>
2018-02-16 16:19:07 -0800

    added more to the program

--- pythonclass/dungeonmaster.py
+++ pythonclass/dungeonmaster.py
@@ -26,14 +26,18 @@ def user_input():
 
 def entrance():
     print("""You wake up, everything is blurry and you do not remember what
-happened to you... You start to gain your vision back, but it feels weird.  
+happened to you... You start to gain your vision back, but it feels weird.
 Although it is blurry, you don't seem to know where you are at.""")
-    print("""You feel around, hopefully finding something that can 
+    print("""You feel around, hopefully finding something that can
 help you out.  You feel something that feels like eyedrops. """)
 
-    userinput = input('Do you take the eyedrops? ')
-    if userinput:
-        print('good for you ')
+    userinput = input('Do you use the eyedrops? ')
+    if userinput == 'yes':
+        print('You USED eyedrops ')
+        print ('Your vision is starting to become clear and you have 0 idea')
+        print ('on where you are.  You see things on the floor it seems like')
+        print ('a shiny sword, a book with a strange engraving, and a locket')
+        print ('Which do you pick up?')
 
 def main():
     entrance()
