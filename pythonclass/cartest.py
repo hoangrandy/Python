ebb89cc186baa30286695cc99c3cfd78a9c820f2
Randy Hoang <rhoang@fb.com>
2018-02-19 14:06:03 -0800

    Testing my carclass module

---
+++ pythonclass/cartest.py
@@ -0,0 +1,12 @@
+from car import Car
+
+my_car = Car('Honda', 'Civic', 28)
+print(my_car.make)
+print(my_car.model)
+print(my_car.fuel_efficiency)
+
+your_car = Car('BMW','M3', 18)
+# Obviously you are doing way better than me
+print(your_car.make)
+print(your_car.model)
+print(your_car.fuel_efficiency)
