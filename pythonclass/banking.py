5cd6ab95046f1036ee5af9efedaefc4bc281278f
Randy Hoang <rhoang@fb.com>
2018-02-19 21:15:57 -0800

    Added le less than function

--- pythonclass/banking.py
+++ pythonclass/banking.py
@@ -1,8 +1,12 @@
 from account import Account
 
 my_account = Account('Randy')
+your_account = Account('Boob')
+your_account.deposit(5)
 my_account.deposit(100)
 print (my_account.balance)
 print(my_account.withdraw(150))
 #print(my_account.balance)
 print(my_account)
+print(your_account)
+print(your_account<=my_account)
