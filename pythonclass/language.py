e1098eab8a0dda41b29de8316e184f6a745dda4e
Randy Hoang <randyviethoang@gmail.com>
2018-02-27 22:51:00 -0800

    Add files via upload

---
+++ pythonclass/language.py
@@ -0,0 +1,28 @@
+# -----------------------------------------------------------------------------
+# Name:        language
+# Purpose:
+#
+# Author:
+# Date:
+# -----------------------------------------------------------------------------
+"""
+module docstring
+
+"""
+import random
+# Enter your imports here
+
+
+def learn(filename):
+    """
+    function docstring
+    """
+    pass  # Take out pass statement and enter your code
+
+
+def sentence_generator(filename, length=10):
+    """
+    function docstring
+    """
+    random.seed(1)  # Set the seed for the random generator - do not remove
+    # Enter your code here
