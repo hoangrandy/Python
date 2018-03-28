4be4f76cf7bee2817089ded2d0d92f9ab3a5bb75
Randy Hoang <randyviethoang@gmail.com>
2018-03-28 08:55:12 -0700

    Delete secondhello.py

--- pythonclass/secondhello.py
+++
@@ -1,8 +0,0 @@
-# -----------------------------------------------------------------------------
-# Name:        firsthello
-# Purpose:     our first Python program
-#
-# Author:      Rula Khayrallah
-# -----------------------------------------------------------------------------
-name = input('Please enter your name: ') #Prompts the user for their name
-print ("Hello", name) #Prints a personalized greeting for the variable name which was given from the input
